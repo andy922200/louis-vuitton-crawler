27f30b3183352b54c73d19c21bfd09cc28f1687d
andy922200-m1-company <andy922200@gmail.com>
2023-01-05 16:45:17 +0800

    1. add error-handling and new export mode

--- main.py
+++ main.py
@@ -29,27 +29,49 @@ class LouisVuittonAPI(object):
             'FR': 'fra-fr',
             'KR': 'kor-kr',
             'JP': 'jpn-jp',
-            'TW': 'zht-tw'
+            'TW': 'zht-tw',
+            'CA': 'eng-ca',
+            'DE': 'deu-de',
+            'IT': 'ita-it',
+            'CN': 'zhs-cn'
+        }
+        self.api_domain = {
+            'UK': 'https://api.louisvuitton.com',
+            'AU': 'https://api.louisvuitton.com',
+            'US': 'https://api.louisvuitton.com',
+            'HK': 'https://api.louisvuitton.com',
+            'FR': 'https://api.louisvuitton.com',
+            'KR': 'https://api.louisvuitton.com',
+            'JP': 'https://api.louisvuitton.com',
+            'TW': 'https://api.louisvuitton.com',
+            'CA': 'https://api.louisvuitton.com',
+            'DE': 'https://api.louisvuitton.com',
+            'IT': 'https://api.louisvuitton.com',
+            'CN': 'https://api-www.louisvuitton.cn'
         }
         self.export_mode = ''
         self.result = {
             'items': [],
             'lv_regions': [],
-            'fetch_time': ''
+            'fetch_time': '',
+            'errors': []
         }
 
         if exportMode.upper() == 'XLSX':
             self.export_mode = 'XLSX'
         elif exportMode.upper() == 'JSON':
-            self.export_mode  = 'JSON'
+            self.export_mode = 'JSON'
+        elif exportMode.upper() == 'BOTH':
+            self.export_mode = 'BOTH'
         else:
             raise ValueError('Invalid Export Mode')
 
         try:
-            for region in regions:  
+            for region in regions:
                 self.result['lv_regions'].append({
                     'lang': self.region_to_lang[region.upper()],
-                    'region': region.upper()
+                    'region': region.upper(),
+                    'api_domain': self.api_domain[region.upper()]
                 })
         except:
             print("Invalid region, correct your spelling or add it.")
@@ -61,17 +83,28 @@ class LouisVuittonAPI(object):
             for skuId in skuNumbers :
                 tempResult = self.fetch_product_info(skuId.upper(), lvRegion)
 
-                time.sleep(0.5)
+                # mock user click speed to avoid errors
+                time.sleep(3)
                 if tempResult is not None :
                     self.result['items'].append(tempResult)
-            time.sleep(2)
-
+            # mock user click speed to avoid errors
+            time.sleep(3)
+        
         if self.export_mode == 'JSON':
             self.exportJson()
-        
+            return
+
         if self.export_mode == 'XLSX':
             self.exportXlsx()
-    
+            return
+
+        if self.export_mode == 'BOTH':
+            self.exportJson()
+            self.exportXlsx()
+            return
+        
+        print('Nothing happened')
+
     def fetch_product_info(self, sku, lvRegion):
         """
         Prints product information.
@@ -87,21 +120,33 @@ class LouisVuittonAPI(object):
         print(f"Region: {lvRegion['region']}")
         print("=" * 60)
 
-        sku_simple_info_url= f"https://api.louisvuitton.com/api/{lvRegion['lang']}/catalog/sku/{sku}/persodetails"
+        sku_simple_info_url= f"{lvRegion['api_domain']}/api/{lvRegion['lang']}/catalog/sku/{sku}/persodetails"
         sku_simple_info_url_text = requests.get(sku_simple_info_url, headers = self.headers).text
         sku_simple_info = json.loads(sku_simple_info_url_text)
 
         if  sku_simple_info.get('errors') and len(sku_simple_info['errors']) > 0:
-            print('No Data')
+            print(sku_simple_info['errors'])
+            self.result['errors'].append(sku_simple_info['errors'])
             return None
 
         if  sku_simple_info['productId']:
-            product_info_url = f"https://api.louisvuitton.com/api/{lvRegion['lang']}/catalog/product/{sku_simple_info['productId']}"
+            product_info_url = f"{lvRegion['api_domain']}/api/{lvRegion['lang']}/catalog/product/{sku_simple_info['productId']}"
             product_info_url_text = requests.get(product_info_url, headers = self.headers).text
             product_info = json.loads(product_info_url_text)
         
             if  product_info:
-                result = {}
+                result = {
+                    'sku': '',
+                    'name': '',
+                    'price': -1,
+                    'currency': '',
+                    'description': '',
+                    'image': [],
+                    'color': '',
+                    'fetchTime': f"{self.result['fetch_time']}Z",
+                    'productId': sku_simple_info['productId'],
+                    'region': lvRegion['region']
+                }
 
                 for prodKey, prodValue in product_info.items():
                     if (prodKey == 'name'):
@@ -118,34 +163,28 @@ class LouisVuittonAPI(object):
 
                             if 'color' in targetValue[0]:
                                 result['color'] = targetValue[0]['color']
-                            else:
-                                result['color'] = ''
 
-                result['fetchTime'] = f"{self.result['fetch_time']}Z"
-                result['productId'] = sku_simple_info['productId']
-                result['region'] = lvRegion['region']
-
-                print("Product Info:")
-                print(f"SKU: {result['sku']}")
-                print(f"PID: {result['productId']}")
-                print(f"Name: {result['name']}")
-                print(f"Price: {result['price']}")
-                print(f"Currency: {result['currency']}")
-                print(f"Description: {result['description']}")
+                print('Done')
                 return result
 
     def exportXlsx(self):
-        result_df = {}
-        for item in self.result['items']:
-            for objKey, objValue in item.items():
-                if objKey in result_df:
-                    result_df[objKey] = [*result_df[objKey], objValue]
-                else:
-                    result_df[objKey] = [objValue]
-        df = pd.DataFrame(result_df)
-        df = df[['sku','productId', 'name','price','currency','color','region','fetchTime','description']]
-        df.index = np.arange(1, len(df)+1)
-        df.to_excel(f'./{self.result["fetch_time"].replace(":","-")}Z.xlsx')
+        if len(self.result['items'])>0:
+            result_df = {}
+            for item in self.result['items']:
+                for objKey, objValue in item.items():
+                    if objKey in result_df:
+                        result_df[objKey] = [*result_df[objKey], objValue]
+                    else:
+                        result_df[objKey] = [objValue]
+            df = pd.DataFrame(result_df)
+            df = pd.concat([df.reset_index(drop=True), pd.DataFrame(df['image'].values.tolist()).reset_index(drop=True).rename(columns=lambda x: f"image_{int(x)+1}")], axis=1)
+            df = df.drop('image', axis=1)
+            df.index = np.arange(1, len(df)+1)
+            df.to_excel(f'./{self.result["fetch_time"].replace(":","-")}Z.xlsx')
+        elif len(self.result['errors'])>0:
+            print('Only errors! Please try again later.')
+        else:
+            print('No Data to export')
     
     def exportJson(self):
         with open(f'./{self.result["fetch_time"].replace(":","-")}Z.json', "w", encoding="utf8") as outfile:
@@ -157,9 +196,9 @@ if __name__ == '__main__':
     print("Louis Vuitton Crawler by Andy Lien (@andy922200)")
     print("=" * 60)
 
-    regionInputs = input("Region Options ( UK,AU,US,FR,HK,KR,JP,TW ) ? Please use \",\" to separate.: ")
+    regionInputs = input("Region Options ( UK,AU,US,FR,HK,KR,JP,TW,CA,DE,IT,CN ) ? Please use \",\" to separate.: ")
     skuNumberInputs = input("SKU Numbers? Please use \",\" to separate. :")
-    exportMode= input("Export Format ( JSON, XLSX ) :")
+    exportMode= input("Export Format ( JSON, XLSX, BOTH ) :")
 
     regions = [regionInput.replace(' ', '') for regionInput in regionInputs.split(',')]
     skuNumbers = [skuNumberInput.replace(' ', '') for skuNumberInput in skuNumberInputs.split(',')]
